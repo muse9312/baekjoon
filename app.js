960b648c8a5150571f8eafdc43ba40dc31bca086
muse9312 <muse9312@naver.com>
2022-12-05 15:04:23 +0900

    fix : 1008 modify

--- app.js
+++ app.js
@@ -27,29 +27,34 @@
 // const [m, ...m_arr] = input.slice(n+1);
 
 
-// const fs = require('fs');
-// let input = require('fs').readFileSync('ex.txt').toString().split('\n');
-// const a = parseInt(input[0]);
-// const b = parseInt(input[1]);
-// console.log(a/b);
-
-
 const fs = require('fs');
 
 const filePath = process.platform === 'linux' ? 0 : 'ex.txt';
-let input = require('fs').readFileSync(filePath).toString().split('\n');
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
-function quadrant(a, b) {
-    if (a > 0 && b > 0) {
-      return 1;
-    } else if (a < 0 && b > 0) {
-      return 2;
-    } else if (a < 0 && b < 0) {
-      return 3;
-    } else if (a > 0 && b < 0) {
-      return 4;
-    }
-  }
+
+console.log(a/b);
+
+
+
+// const fs = require('fs');
+
+// const filePath = process.platform === 'linux' ? 0 : 'ex.txt';
+// let input = require('fs').readFileSync(filePath).toString().split('\n');
+// const a = parseInt(input[0]);
+// const b = parseInt(input[1]);
+// function quadrant(a, b) {
+//     if (a > 0 && b > 0) {
+//       return 1;
+//     } else if (a < 0 && b > 0) {
+//       return 2;
+//     } else if (a < 0 && b < 0) {
+//       return 3;
+//     } else if (a > 0 && b < 0) {
+//       return 4;
+//     }
+//   }
   
-  console.log(quadrant(a, b));
+//   console.log(quadrant(a, b));
