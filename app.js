94f04550e04d159f435507817576d79b49f656cd
muse9312 <muse9312@naver.com>
2022-12-05 16:03:03 +0900

    fix : app.js modify

--- app.js
+++ app.js
@@ -30,7 +30,7 @@
 const fs = require('fs');
 
 const filePath = process.platform === 'linux' ? 0 : 'ex.txt';
-const input = fs.readFileSync(filePath).toString().trim().split(" ");
+const input = fs.readFileSync(filePath).toString().split(" ");
 
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
